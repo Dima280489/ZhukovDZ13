a9a4a7c418aef9d5e385e9b41da64fab67e02b8e
Neto test <demon_4@mail.ru>
2023-10-23 21:18:33 +0300

    First commit

--- src/test/java/ProductManager/ShopRepositoryTest.java
+++ src/test/java/ProductManager/ShopRepositoryTest.java
@@ -31,10 +31,9 @@ public class ShopRepositoryTest {
         repo.add(product1);
         repo.add(product2);
         repo.add(product3);
-        repo.remove(5);
-        Product[] expected = {product1, product2, product3};
-        Product[] actual = repo.findAll();
 
-        Assertions.assertArrayEquals(expected, actual);
+        Assertions.assertThrows(NotFoundException.class,
+                () -> repo.remove(5)
+        );
     }
 }
